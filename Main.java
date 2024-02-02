c053a3c74056ee6de692206e5fb9ae39a7cd92d2
DennisApaydin <Dennis.Apaydin@edu.fhdw.de>
2024-02-02 12:51:49 +0100

    No message

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     static final int SIZE = 10;
-    static final String ENTER_SHIP_COORDINATE_PROMPT = "Geben Sie die %s für eine Schiff der Länge %d ein:";
+    static final String ENTER_SHIP_COORDINATE_PROMPT = "Geben Sie die %skoordinaten für eine Schiff der Länge %d ein:";
 
     static int distance(final Coordinate start, final Coordinate end) {
         return Math.abs(start.column() - end.column()) + Math.abs(start.row() - end.row());
@@ -11,7 +11,7 @@ public class Main {
         return new Coordinate(Utility.getRandomInt(SIZE), Utility.getRandomInt(SIZE));
     }
 
-    static boolean onOneLine(final Coordinate start, final Coordinate end){
+    static boolean onOneLine(final Coordinate start, final Coordinate end) {
         return (start.row() == end.row() || start.column() == end.column());
     }
 
@@ -19,13 +19,12 @@ public class Main {
         System.out.println("   +-+-+-+-+-+-+-+-+-+-+      +-+-+-+-+-+-+-+-+-+-+");
     }
 
-    static int getMaxSurroundingColumn(final Coordinate start, final Coordinate end){
+    static int getMaxSurroundingColumn(final Coordinate start, final Coordinate end) {
         int index = Math.max(start.column(), end.column());
 
-        if(index == 9){
+        if (index == 9) {
             return index;
-        }
-        else {
+        } else {
             return index++;
         }
     }
@@ -33,10 +32,9 @@ public class Main {
     static int getMaxSurroundingRow(final Coordinate start, final Coordinate end) {
         int index = Math.max(start.row(), end.row());
 
-        if(index == 9){
+        if (index == 9) {
             return index;
-        }
-        else {
+        } else {
             return index++;
         }
     }
@@ -56,8 +54,7 @@ public class Main {
 
         if (index == 1) {
             return index;
-        }
-        else {
+        } else {
             return index--;
         }
     }
@@ -76,34 +73,115 @@ public class Main {
             return false;
         }
 
-        if(String.valueOf(input.charAt(0)).matches("[A-J]")) {
-            if (String.valueOf(input.charAt(1)).matches("[1-10]")) {
-                return true;
-            }
+        if (input.toUpperCase().matches("[A-J](10|[1-9)]")) {
+            return true;
         }
-
         return false;
     }
 
     static String getStartCoordinatePrompt(final int length) {
-        return String.format(ENTER_SHIP_COORDINATE_PROMPT, "Startkoordinaten", length);
+        return String.format(ENTER_SHIP_COORDINATE_PROMPT, "Start", length);
     }
 
     static String getEndCoordinatePrompt(final int length) {
-        return String.format(ENTER_SHIP_COORDINATE_PROMPT, "Endkoordinaten", length);
+        return String.format(ENTER_SHIP_COORDINATE_PROMPT, "End", length);
     }
 
     public static void main(String[] args) {
+        test();
+    }
+
+
+    static void test() {
         //Coordinate a = new Coordinate (SIZE, SIZE);
         //System.out.println( getMaxSurroundingColumn(a, a) );
-        System.out.println(getMaxSurroundingColumn(new Coordinate(1,2), new Coordinate(2,3)));
-        System.out.println(getMaxSurroundingColumn(new Coordinate(9,2), new Coordinate(2,3)));
-        System.out.println(getMinSurroundingColumn(new Coordinate(1,2), new Coordinate(2,3)));
-        System.out.println(getMinSurroundingColumn(new Coordinate(0,2), new Coordinate (2,3)));
-        System.out.println(getMaxSurroundingRow(new Coordinate(1,2),new Coordinate (2,3)));
-        System.out.println(getMaxSurroundingRow(new Coordinate(1,9), new Coordinate (2,3)));
-        System.out.println(getMinSurroundingRow(new Coordinate(1,2),new Coordinate (2,3)));
-        System.out.println(getMinSurroundingRow(new Coordinate(1,0), new Coordinate (2,3)));
+        System.out.println(getMaxSurroundingColumn(new Coordinate(1, 2), new Coordinate(2, 3)));
+        System.out.println(getMaxSurroundingColumn(new Coordinate(9, 2), new Coordinate(2, 3)));
+        System.out.println(getMinSurroundingColumn(new Coordinate(1, 2), new Coordinate(2, 3)));
+        System.out.println(getMinSurroundingColumn(new Coordinate(0, 2), new Coordinate(2, 3)));
+        System.out.println(getMaxSurroundingRow(new Coordinate(1, 2), new Coordinate(2, 3)));
+        System.out.println(getMaxSurroundingRow(new Coordinate(1, 9), new Coordinate(2, 3)));
+        System.out.println(getMinSurroundingRow(new Coordinate(1, 2), new Coordinate(2, 3)));
+        System.out.println(getMinSurroundingRow(new Coordinate(1, 0), new Coordinate(2, 3)));
         System.out.println(toCoordinate("c5"));
     }
-}
+
+    static boolean istZahlGeradeUngerade(int input) {
+        if (input % 2 != 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+        static void welcherWochentagIstHeute(int input){
+            if (input == 1) {
+                System.out.println("Montag");
+            } else if (input == 2) {
+                System.out.println("Dienstag");
+            } else if (input == 3) {
+                System.out.println("Mittwoch");
+            } else if (input == 4) {
+                System.out.println("Donnerstag");
+            } else if (input == 5) {
+                System.out.println("Freitag");
+            } else if (input == 6) {
+                System.out.println("Samstag");
+            } else if (input == 7) {
+                System.out.println("Sonntag");
+            }
+        }
+
+        static void welcherMonat(int input) {
+                switch (input) {
+                    case 1:
+                        System.out.println("Januar");
+                        break;
+
+                    case 2:
+                        System.out.println("Februar");
+                        break;
+
+                    case 3:
+                        System.out.println("März");
+                        break;
+
+                    case 4:
+                        System.out.println("April");
+                        break;
+
+                    case 5:
+                        System.out.println("Mai");
+                        break;
+
+                    case 6:
+                        System.out.println("Juni");
+                        break;
+
+                    case 7:
+                        System.out.println("Juli");
+                        break;
+
+                    case 8:
+                        System.out.println("August");
+                        break;
+
+                    case 9:
+                        System.out.println("September");
+                        break;
+
+                    case 10:
+                        System.out.println("Oktober");
+                        break;
+
+                    case 11:
+                        System.out.println("November");
+                        break;
+
+                    case 12:
+                        System.out.println("Dezember");
+                        break;
+
+                }
+        }
+    }
